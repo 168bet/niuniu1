1fea0ce467546a9a96f74ae32689d2dab1b463cf
WorldCasino <m0o0m@outlook.com>
2017-11-27 17:04:23 +0800

    Initial commit

---
+++ application/member/controller/Index.php
@@ -0,0 +1,15 @@
+<?php 
+namespace app\member\controller;
+use think\Controller;
+use think\Db;
+use think\Validate;
+use think\Session;
+class Index extends Common
+{
+	public function index()
+	{
+		
+		return $this->fetch();
+	}
+	
+}
